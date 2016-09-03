beb80b33d0e08e505118b36dcb5428dd5d5a1840
Noemi Quezada <noemi01@csu.fullerton.edu>
2016-09-03 00:32:33 -0700

    Created a new snake.js file to store the snake object and added the reference in the index.html

--- sketch.js
+++ sketch.js
@@ -10,20 +10,3 @@ function draw() {
     s.update();
     s.show();
 }
-
-function Snake() {
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
-
-    this.update = function() {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
-    }
-
-    this.show = function() {
-        fill(255);
-        rect(this.x, this.y, 10, 10);
-    }
-}
