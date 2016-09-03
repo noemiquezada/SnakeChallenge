974e2ab33c8167f03ef448c51085a58474014161
Noemi Quezada <noemi01@csu.fullerton.edu>
2016-09-03 14:44:58 -0700

    Updated the update and show function so that the tail of the snake gets drawn as it eats food

--- snake.js
+++ snake.js
@@ -3,6 +3,9 @@ function Snake() {
     this.y = 0;
     this.xDirection = 1;
     this.yDirection = 0;
+    // Tail length not including the head
+    this.total = 0;
+    this.tail = [];
 
     this.direction = function(x,y) {
         this.xDirection = x;
@@ -10,6 +13,14 @@ function Snake() {
     }
 
     this.update = function() {
+        if (this.total === this.tail.length) {
+            for (var i = 0; i < this.tail.length-1; i++) {
+                this.tail[i] = this.tail[i+1];
+            }
+        }
+
+        this.tail[this.total-1] = createVector(this.x, this.y);
+
         this.x = this.x + this.xDirection*scl;
         this.y = this.y + this.yDirection*scl;
 
@@ -20,12 +31,16 @@ function Snake() {
     this.show = function() {
         stroke(255);
         fill(255);
+        for (var i = 0; i < this.tail.length; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
         rect(this.x, this.y, scl, scl);
     }
 
     this.eat = function(pos) {
         var d = dist(this.x, this.y, pos.x, pos.y);
         if (d < 1) {
+            this.total++;
             return true;
         } else {
             return false;
